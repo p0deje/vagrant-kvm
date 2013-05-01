42df01ec6dd386fed3bc103dd84b03796e230a8c
Alex Rodionov <p0deje@gmail.com>
2013-05-01 18:33:02 +0700

    qcow2 support

--- lib/vagrant-kvm/driver/driver.rb
+++ lib/vagrant-kvm/driver/driver.rb
@@ -88,12 +88,12 @@ module VagrantPlugins
           # copy volume to storage pool
           box_disk = definition.disk
           new_disk = File.basename(box_disk, File.extname(box_disk)) + "-" +
-            Time.now.to_i.to_s + ".img"
+            Time.now.to_i.to_s + ".qcow2"
           @logger.info("Copying volume #{box_disk} to #{new_disk}")
           old_path = File.join(File.dirname(xml), box_disk)
           new_path = File.join(path, new_disk)
           # we use qemu-img convert to preserve image size
-          system("qemu-img convert -p #{old_path} -O raw #{new_path}")
+          system("qemu-img convert -p #{old_path} -O qcow2 #{new_path}")
           @pool.refresh
           volume = @pool.lookup_volume_by_name(new_disk)
           definition.disk = volume.path
